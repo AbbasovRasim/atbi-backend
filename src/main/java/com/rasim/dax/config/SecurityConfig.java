fb7c03d9f2e08f52c709201f760947e900018163
AbbasovRasim <ras.abbasov@gmail.com>
2026-05-13 14:58:53 +0400

    project updates

--- src/main/java/com/rasim/dax/config/SecurityConfig.java
+++ src/main/java/com/rasim/dax/config/SecurityConfig.java
@@ -1,65 +1,34 @@
 package com.rasim.dax.config;
 
 import com.rasim.dax.security.JwtFilter;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
 
-    public SecurityConfig(
-            JwtFilter jwtFilter
-    ) {
-
+    public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http
-    ) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
 
-                .cors(Customizer.withDefaults())
-
-                .sessionManagement(session -> session
-
-                        .sessionCreationPolicy(
-                                SessionCreationPolicy.STATELESS
-                        )
-                )
-
                 .authorizeHttpRequests(auth -> auth
-
-                        .requestMatchers(
-                                "/auth/**"
-                        ).permitAll()
-
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/incidents/**").permitAll()
                         .anyRequest().authenticated()
-                )
-
-                .httpBasic(httpBasic ->
-                        httpBasic.disable()
-                )
-
-                .formLogin(form ->
-                        form.disable()
                 )
 
                 .addFilterBefore(
