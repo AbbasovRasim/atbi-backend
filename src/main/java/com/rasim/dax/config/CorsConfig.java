2a58d9cff4243105a530002bbf596d20097f708b
AbbasovRasim <ras.abbasov@gmail.com>
2026-05-16 09:57:53 +0400

    update security config

--- src/main/java/com/rasim/dax/config/CorsConfig.java
+++ src/main/java/com/rasim/dax/config/CorsConfig.java
@@ -14,9 +14,10 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.addAllowedOriginPattern("*");
+        config.addAllowedOriginPattern("https://atbi-system.vercel.app");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
